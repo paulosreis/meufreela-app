8bda2a2eb51f6e2acc9b7485bba1f9de2a926592
Paulo Guilherme S Reis <paulosantos--@live.com>
2023-06-20 19:39:28 -0400

    feat: Implementa bottom navigation, fab e pagina de perfil

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -23,22 +29,39 @@ public class MainActivity extends AppCompatActivity {
 
         FirebaseAuth auth = FirebaseAuth.getInstance();
         FirebaseUser user = auth.getCurrentUser();
-        Button logoutButton = findViewById(R.id.logoutButton);
-        TextView userEmail = findViewById(R.id.userEmail);
-        if (user == null){
-            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-            startActivity(intent);
-            finish();
-        }
-        else {
-            userEmail.setText(user.getEmail());
-        }
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        logoutButton.setOnClickListener(view -> {
-            FirebaseAuth.getInstance().signOut();
-            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        FloatingActionButton fab = findViewById(R.id.floating_action_button);
+        fab.setOnClickListener(this::showPopup);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_app_bar, menu);
+        MenuItem profileItem = menu.findItem(R.id.profile);
+        profileItem.setOnMenuItemClickListener(item -> {
+            Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
             startActivity(intent);
-            finish();
+            return true;
         });
+        return true;
+    }
+
+
+
+    private void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        popup.setOnMenuItemClickListener(item -> {
+            // Ação para a segunda opção
+            if (item.getItemId() == R.id.popup_option_1) {
+                // Ação para a primeira opção
+                return true;
+            } else return item.getItemId() == R.id.popup_option_2;
+        });
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.popup_menu, popup.getMenu());
+        popup.show();
     }
 }
