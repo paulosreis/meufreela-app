b190ca6ba03e22b1978e805b1444ac00d7bb5ce2
Paulo Guilherme S. Reis <paulosantos--@live.com>
2023-06-23 16:53:57 -0400

    refact: Reestrutura parte do projeto para MVVM e implementa navegação via fragmentos

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,38 +1,69 @@
 package com.example.myapplication;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.PopupMenu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
+import com.example.myapplication.databinding.ActivityMainBinding;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        com.example.myapplication.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+//        setContentView(R.layout.activity_main);
 
         MaterialToolbar topAppBar = findViewById(R.id.topAppBar);
         setSupportActionBar(topAppBar);
 
 
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        FirebaseUser user = auth.getCurrentUser();
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-
         FloatingActionButton fab = findViewById(R.id.floating_action_button);
         fab.setOnClickListener(this::showPopup);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_home, R.id.navigation_freelancers, R.id.navigation_jobs).build();
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host);
+        assert navHostFragment != null;
+        NavController navController = navHostFragment.getNavController();
+
+        NavigationUI.setupWithNavController(binding.navView, navController);
+
+        navView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.navigation_home) {
+                navController.navigate(R.id.navigation_home);
+                return true;
+            } else if (itemId == R.id.navigation_freelancers) {
+                navController.navigate(R.id.navigation_freelancers);
+                return true;
+            } else if (itemId == R.id.navigation_jobs) {
+                navController.navigate(R.id.navigation_jobs);
+                return true;
+            }
+            return false;
+        });
 
 
     }
@@ -50,13 +81,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     private void showPopup(View v) {
         PopupMenu popup = new PopupMenu(this, v);
         popup.setOnMenuItemClickListener(item -> {
             // Ação para a segunda opção
             if (item.getItemId() == R.id.popup_option_1) {
                 // Ação para a primeira opção
+                Intent intent = new Intent(MainActivity.this, NewFreelancer.class);
+                startActivity(intent);
                 return true;
             } else return item.getItemId() == R.id.popup_option_2;
         });
---
+++ app/src/main/java/com/example/myapplication/ui/freelancers/FreelancersFragment.java
@@ -0,0 +1,69 @@
+package com.example.myapplication.ui.freelancers;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.myapplication.databinding.FragmentFreelancersBinding;
+import com.example.myapplication.model.Freelancer;
+
+import java.util.ArrayList;
+
+public class FreelancersFragment extends Fragment {
+
+    private FragmentFreelancersBinding binding;
+    private FreelancerAdapter freelancerAdapter;
+    private FreelancersViewModel freelancersViewModel;
+    private Freelancer freelancerToDelete;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentFreelancersBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        freelancerAdapter = new FreelancerAdapter(getContext(), new ArrayList<>());
+        binding.freelancerRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.freelancerRecyclerView.setAdapter(freelancerAdapter);
+
+        freelancersViewModel = new ViewModelProvider(this).get(FreelancersViewModel.class);
+        freelancersViewModel.getFreelancerListLiveData().observe(getViewLifecycleOwner(), freelancers -> freelancerAdapter.setFreelancers(freelancers));
+
+        freelancerAdapter.setOnDeleteCLickListener(freelancer -> {
+            freelancerToDelete = freelancer;
+            showDeleteConfirmationDialog();
+        });
+
+        freelancerAdapter.setOnDeleteConfirmationListener(freelancer -> freelancersViewModel.deleteFreelancer(freelancer));
+
+
+        return root;
+    }
+
+
+    private void showDeleteConfirmationDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Confirmação de exclusão");
+        builder.setMessage("Deseja excluir este freelancer?");
+        builder.setPositiveButton("Sim", (dialog, which) -> {
+            if (freelancersViewModel != null && freelancerToDelete != null) {
+                freelancersViewModel.deleteFreelancer(freelancerToDelete);
+            }
+        });
+        builder.setNegativeButton("Cancelar", null);
+        builder.create().show();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
